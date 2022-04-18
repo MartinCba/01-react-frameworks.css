dc2ec0cbc2a5ae7342fed49d7490717089e29ce6
MartinCba <martinalejandrohernandez1@gmail.com>
2022-04-18 17:30:04 -0300

    subir a repo remoto lo aprendido en el curso

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Bootstrap from "./components/Bootstrap";
+import Bulma from "./components/Bulma";
+
+function App() {
+  return (
+    <div>
+      <h1>Frameworks CSS con React</h1>
+      <br />
+      {/* {Bootstrap /} */}
+      {/* {<Bulma />} */}
+    </div>
+  );
+}
+
+export default App;
